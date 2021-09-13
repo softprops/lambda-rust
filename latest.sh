10a671d1cbae4ca236edbebc1f1c946f3df3749f
Jeremiah Russell <jrussell@jerus.ie>
2021-09-13 19:28:17 +0100

    Use gh instead of action

--- latest.sh
+++ latest.sh
@@ -9,11 +9,7 @@ source /cargo/env
 rustup toolchain install stable --profile=minimal
 STABLE=$(rustup check | grep stable | grep -E "[0-9]+\.[0-9]+\.[0-9]+" -o)
 DEFAULT=$(rustup show | grep -m 1 default | grep -E "[0-9]+\.[0-9]+\.[0-9]+" -o)
-echo "::set-output name=stable_rust::${STABLE}"
-echo "${STABLE}"
-if [ "${STABLE}" == "${DEFAULT}" ]; then
-  exit 0
-else 
-  gh issue create --title "Time to update to Rust ${STABLE}" --body "Build update for Rust ${STABLE}"
-  exit 1
+
+if [ "${STABLE}" == "${DEFAULT}" ]; then exit 0
+    else exit 1
 fi
