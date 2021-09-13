031cf8d3cb1815b3565640c98becd06b72bd64d1
Jeremiah Russell <jrussell@jerus.ie>
2021-09-13 12:09:23 +0100

    Create an issue

--- latest.sh
+++ latest.sh
@@ -9,7 +9,10 @@ source /cargo/env
 rustup toolchain install stable --profile=minimal
 STABLE=$(rustup check | grep stable | grep -E "[0-9]+\.[0-9]+\.[0-9]+" -o)
 DEFAULT=$(rustup show | grep -m 1 default | grep -E "[0-9]+\.[0-9]+\.[0-9]+" -o)
+echo "::set-output name=stable_rust::$STABLE"
 
-if [ "${STABLE}" == "${DEFAULT}" ]; then exit 0
-    else exit 1
+if [ "${STABLE}" == "${DEFAULT}" ]; then
+  exit 0
+else 
+  exit 1
 fi
